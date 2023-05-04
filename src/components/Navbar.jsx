0d38d608002817d0e17021cce5c8ff44b2d3cf83
Lykaios-SD <58195681+Lykaios-SD@users.noreply.github.com>
2023-05-04 13:55:14 +0200

    Cambios de filtrado y post-process

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,114 +1,57 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import useAuth from "../hooks/useAuth";
-import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import useAuth from "../hooks/useAuth";
 import useServer from "../hooks/useServer";
 import { apiURL } from "../config";
 
 function NavBar() {
-  const {get} = useServer()
   const { isAuthenticated, logout } = useAuth();
+  const { get } = useServer();
   const [profile, setProfile] = useState(null);
 
-  const getProfile = async () => {
-    const response = await get({ url: "/profile" })
-    setProfile(response.data.data)
-    setProfile(response);
+  const fetchProfile = async () => {
+    const response = await get({ url: "/profile" });
+    setProfile(response.data.data);
   };
 
   useEffect(() => {
-    getProfile();
+    fetchProfile();
   }, []);
 
-  if (profile) {
-    return (
-      <>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand to="#logo">InsightHub</Navbar.Brand>
-            <Nav>
-            {/* Enlace a la página de inicio */}
-            <NavLink to="/">
-              Inicio
-            </NavLink>
-            
-            {/* Si el usuario está autenticado, mostrar enlace para publicar */}
-            {isAuthenticated && (
-              <NavLink to="/send">
-                Publicar
-              </NavLink>
-            )}
-            {isAuthenticated && (
-              <NavLink to="/profile">
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand to="#logo">InsightHub</Navbar.Brand>
+        <Nav>
+          <NavLink to="/">Inicio</NavLink>
+
+          {isAuthenticated && (
+            <NavLink to="/send">Publicar</NavLink>
+          )}
+
+          {isAuthenticated && profile && (
+            <NavLink to="/profile">
               Perfil
-              {/* <img className='avatars' src={`${apiURL}/avatars/${profile.avatar}`}/>   */}
-              </NavLink>
-            )}
-      
-            {/* Si el usuario está autenticado, mostrar enlace para cerrar sesión, de lo contrario mostrar enlace para iniciar sesión */}
-            {isAuthenticated ? (
-              <NavLink to="/login" onClick={logout}>
-                Cerrar Sesión
-              </NavLink>
-            ) : (
-              <NavLink to="/login">
-                Inicio Sesión
-              </NavLink>
-            )
-            }
-            </Nav>
-          </Container>
-        </Navbar>
-      </>
-    );
-  }
-
-  if (profile) {
-    return (
-      <>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand to="#home"></Navbar.Brand>
-            <Nav>
-            {/* Enlace a la página de inicio */}
-            <NavLink to="/">
-              Inicio
+              <img
+                src={`${apiURL}/avatars/${profile.avatar}`}
+                style={{ width: "8%" }}
+                alt="avatar"
+              />
             </NavLink>
-            
-            {/* Si el usuario está autenticado, mostrar enlace para publicar */}
-            {isAuthenticated && (
-              <NavLink to="/send">
-                Publicar
-              </NavLink>
-            )}
-            {isAuthenticated && (
-              <NavLink to="/profile">
-              Perfil
-              <img src={`${apiURL}/avatars/${profile.avatar}`} style={{width: "8%"}}/>  
-              </NavLink>
-            )}
-      
-            {/* Si el usuario está autenticado, mostrar enlace para cerrar sesión, de lo contrario mostrar enlace para iniciar sesión */}
-            {isAuthenticated ? (
-              <NavLink to="/login" onClick={logout}>
-                Cerrar Sesión
-              </NavLink>
-            ) : (
-              <NavLink to="/login">
-                Inicio Sesión
-              </NavLink>
-            )
-            }
-            </Nav>
-          </Container>
-        </Navbar>
-      </>
-    );
-  }
+          )}
 
+          {isAuthenticated ? (
+            <NavLink to="/login" onClick={logout}>
+              Cerrar Sesión
+            </NavLink>
+          ) : (
+            <NavLink to="/login">Inicio Sesión</NavLink>
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
 
-
-} 
 export default NavBar;
